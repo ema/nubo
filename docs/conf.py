1111736fe7af5bf512bba6168723e2a885c0f1df
Emanuele Rocca <ema@linux.it>
2013-04-13 15:51:14 +0200

    Change homepage title

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Nubo'
+html_title = 'A command line interface to multiple cloud providers'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
