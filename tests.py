04131af35ce02b8140bc7fb34abba484383f6502
Emanuele Rocca <ema@linux.it>
2013-05-21 20:02:35 +0200

    Increase test coverage (nubo.clouds.base)

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from nubo import config
+from nubo import remote
+
 from nubo.clouds import base
 
 from nubo.clouds.ec2 import AmazonEC2
@@ -35,6 +38,27 @@ class BaseTest(unittest.TestCase):
         self.assertEquals(OpenNebula, one)
         self.assertEquals('OPENNEBULA', one.PROVIDER_NAME)
 
+    def test_read_config(self):
+        self.assertEquals(dict, type(config.read_config()))
+
+    def test_read_no_conffile(self):
+        oldfile = config.CONFFILE
+
+        config.CONFFILE = '/does_not_exist'
+        self.assertEquals({}, config.read_config())
+
+        config.CONFFILE = oldfile
+
+    def test_write_config_no_new_values(self):
+        old_config = config.read_config()
+        new_config = config.write_config({})
+        self.assertEquals(old_config, new_config)
+
+    def test_remote_object(self):
+        host = remote.RemoteHost('127.0.0.1', 'dummy')
+        self.assertEquals(host.host, '127.0.0.1')
+        self.assertEquals(host.private_key, 'dummy')
+
 class BaseCloudTest(unittest.TestCase):
 
     def setUp(self):
@@ -66,12 +90,32 @@ class BaseCloudTest(unittest.TestCase):
 
         self.assertEquals('root', self.cloud.login_as)
 
+    def test_init_no_private_key(self):
+        self.cloud = self.CloudClass()
+        self.failUnless(self.cloud.ssh_public_key.endswith('.pub'))
+
+    def test_init_wrong_provider_name(self):
+        self.CloudClass.PROVIDER_NAME = 'WRONG_PROVIDER'
+        self.assertRaises(Exception, self.CloudClass)
+
     def test_startup(self):
         new_node = self.cloud.startup({})
 
         self.assertEquals(dict, type(new_node))
         self.assertEquals('RUNNING', new_node['state'])
 
+    def test_node2dict(self):
+        node = self.cloud.driver.list_nodes()[0]
+        expected = {
+            'extra': {'foo': 'bar'},
+            'id': '1',
+            'name': 'dummy-1',
+            'private_ips': [],
+            'public_ips': ['127.0.0.1'],
+            'state': 'RUNNING'
+        }
+        self.assertEquals(expected, base.node2dict(node))
+        
     def test_is_running(self):
         self.failUnless(self.cloud.is_running(node_id='1'))
         self.failIf(self.cloud.is_running(node_id='42'))
@@ -106,10 +150,26 @@ class BaseCloudTest(unittest.TestCase):
         self.assertEquals('Ubuntu 9.04', images[1].name)
         self.assertEquals('Slackware 4', images[2].name)
 
+    def test_list_images_with_limit(self):
+        images = self.cloud.list_images(limit=2)
+        self.assertEquals(2, len(images))
+        self.assertEquals('Ubuntu 9.10', images[0].name)
+        self.assertEquals('Ubuntu 9.04', images[1].name)
+
+    def test_list_images_with_keyword(self):
+        images = self.cloud.list_images(keyword='slackware')
+        self.assertEquals(1, len(images))
+        self.assertEquals('Slackware 4', images[0].name)
+
     def test_list_sizes(self):
         sizes = self.cloud.list_sizes()
         expected = [ 'Small', 'Medium', 'Big', 'XXL Big']
         self.assertEquals(expected, sizes)
+
+    def test_deploy_on_base_class(self):
+        base.BaseCloud.PROVIDER_NAME = 'DUMMY'
+        cloud = base.BaseCloud()
+        self.assertRaises(NotImplementedError, cloud.deploy)
 
 if __name__ == "__main__":
     unittest.main()
